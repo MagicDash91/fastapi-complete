fe31c0f641fff7e5c6bfc98dd23f5a47048f05bb
MichaelW <75797622+MagicDash91@users.noreply.github.com>
2024-09-22 16:03:24 +0700

    Update webapp.py

--- fastapi_complete/webapp.py
+++ fastapi_complete/webapp.py
@@ -852,5 +852,5 @@ def save_to_json(question_responses):
 
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    port = int(os.environ.get("PORT", 8000)) 
+    uvicorn.run(app, host="0.0.0.0", port=port)
